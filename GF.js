01c63f5e637617b33b66d95283a6c6444840b9bf
Haruki <harukixsira@gmail.com>
2017-03-02 14:49:24 -0600

    added 1 javascript and edited some padding.

---
+++ GF.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ls7004jg on 3/2/2017.
+ */
+<script>
+function appear() {
+    var allh2 = document.getElementsByTagName("h2");
+
+    for (var h2 = 0 ; h2 < allh2.length; h2++) {
+        allh2[h2].style.color="blue";
+    }
+}
+</script>
